2b880f5d7f9506cc9a307648bb351d28c2157a85
Christian Cano <christian_k106@hotmail.com>
2020-09-22 12:21:51 -0500

    Update footer-functions.js
    Se agregó código para controlar el evento por defecto de la tecla ENTER en los formularios, evitando el envío de formularios con su pulsación.

--- js/footer-functions.js
+++ js/footer-functions.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
     $("#table-facturas").DataTable();
 });
 
+$('form input').keydown(function (e) {
+    if (e.keyCode == 13) {
+        var inputs = $(this).parents("form").eq(0).find(":input");
+        if (inputs[inputs.index(this) + 1] != null) {                    
+            inputs[inputs.index(this) + 1].focus();
+        }
+        e.preventDefault();
+        return false;
+    }
+});
+
 function crear_cookie(name, value, days2expire, path) {
     var date = new Date();
     date.setTime(date.getTime() + (days2expire * 24 * 60 * 60 * 1000));
@@ -17,25 +28,25 @@ function crear_cookie(name, value, days2expire, path) {
     document.cookie = name + '=' + value + ';' +
       'expires=' + expires + ';' +
       'path=' + path + ';';
-  }
+}
   
-  function leer_cookie(name) {
-    var cookie_value = "",
-      current_cookie = "",
-      name_expr = name + "=",
-      all_cookies = document.cookie.split(';'),
-      n = all_cookies.length;
-  
-    for (var i = 0; i < n; i++) {
-      current_cookie = all_cookies[i].trim();
-      if (current_cookie.indexOf(name_expr) == 0) {
-        cookie_value = current_cookie.substring(name_expr.length, current_cookie.length);
-        break;
-      }
+function leer_cookie(name) {
+  var cookie_value = "",
+    current_cookie = "",
+    name_expr = name + "=",
+    all_cookies = document.cookie.split(';'),
+    n = all_cookies.length;
+
+  for (var i = 0; i < n; i++) {
+    current_cookie = all_cookies[i].trim();
+    if (current_cookie.indexOf(name_expr) == 0) {
+      cookie_value = current_cookie.substring(name_expr.length, current_cookie.length);
+      break;
     }
-    return cookie_value;
   }
+  return cookie_value;
+}
   
-  function eliminar_cookie(name) {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-  }
+function eliminar_cookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+}
